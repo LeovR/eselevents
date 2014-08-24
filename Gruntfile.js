3053f5f5280aa1d6ee22ced93c1282d13becdc7c
Jan Marten <jan.marten@gmx.de>
2014-08-24 12:51:01 +0200

    Ignore events folder for revisioning

--- Gruntfile.js
+++ Gruntfile.js
@@ -180,7 +180,7 @@ module.exports = function (grunt) {
           src: [
             '<%= yeoman.dist %>/scripts/{,*/}*.js',
             '<%= yeoman.dist %>/styles/{,*/}*.css',
-            '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+            '<%= yeoman.dist %>/images/{,!events*/}*.{png,jpg,jpeg,gif,webp,svg}',
             '<%= yeoman.dist %>/fonts/*'
           ]
         }
