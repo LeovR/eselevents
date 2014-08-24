3673e514f16f29358ac32416f569a0db798c3543
Jan Marten <jan.marten@gmx.de>
2014-08-24 08:51:14 +0200

    Set assetsDirs in order to process images urls in views correctly

--- Gruntfile.js
+++ Gruntfile.js
@@ -195,7 +195,8 @@ module.exports = function (grunt) {
       html: ['<%= yeoman.dist %>/{,*/}*.html'],
       css: ['<%= yeoman.dist %>/styles/{,*/}*.css'],
       options: {
-        dirs: ['<%= yeoman.dist %>']
+        dirs: ['<%= yeoman.dist %>'],
+        assetsDirs: ['<%= yeoman.dist %>','<%= yeoman.dist %>/images']
       }
     },
     imagemin: {
